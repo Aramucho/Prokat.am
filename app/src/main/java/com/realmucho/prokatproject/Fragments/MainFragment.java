bddeff3720307e929bcb03d99d547c95b5f69520
Sona Chitchyan <chitchyan.sona@gmail.com>
2017-05-22 15:39:31 +0400

    Initial

--- app/src/main/java/com/realmucho/prokatproject/Fragments/MainFragment.java
+++ app/src/main/java/com/realmucho/prokatproject/Fragments/MainFragment.java
@@ -43,8 +43,8 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        final Intent[] intent = new Intent[3];
-        Handler handler;
+        Intent intent;
+
         final int REQ_GOODS = 1;
         final int REQ_TRANSPORT = 2;
         final int REQ_SERVICE = 3;
@@ -53,53 +53,30 @@ public class MainFragment extends Fragment implements View.OnClickListener {
         switch (id) {
 
             case R.id.goods_image:
-                handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
 
-                        intent[0] = new Intent(getContext(), CategoryActivity.class);
-                        intent[0].putExtra("code", REQ_GOODS);
-                        startActivity(intent[0]);
-                    }
-                }, 1000);
+
+                intent = new Intent(getContext(), CategoryActivity.class);
+                intent.putExtra("code", REQ_GOODS);
+                startActivity(intent);
 
 
                 break;
             case R.id.transport_image:
-                handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        intent[1] = new Intent(getContext(), CategoryActivity.class);
-                        intent[1].putExtra("code", REQ_TRANSPORT);
-                        startActivity(intent[0]);
-                    }
-                }, 1000);
+                intent = new Intent(getContext(), CategoryActivity.class);
+                intent.putExtra("code", REQ_TRANSPORT);
+                startActivity(intent);
 
                 break;
             case R.id.service_image:
-                handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        intent[2] = new Intent(getContext(), CategoryActivity.class);
-                        intent[2].putExtra("code", REQ_SERVICE);
-                        startActivity(intent[0]);
-                    }
-                }, 1000);
+                intent = new Intent(getContext(), CategoryActivity.class);
+                intent.putExtra("code", REQ_SERVICE);
+                startActivity(intent);
 
                 break;
             case R.id.realty_image:
-                handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        intent[3] = new Intent(getContext(), CategoryActivity.class);
-                        intent[3].putExtra("code", REQ_REALTY);
-                        startActivity(intent[0]);
-                    }
-                }, 1000);
+                intent = new Intent(getContext(), CategoryActivity.class);
+                intent.putExtra("code", REQ_REALTY);
+                startActivity(intent);
 
                 break;
 
@@ -109,15 +86,14 @@ public class MainFragment extends Fragment implements View.OnClickListener {
 
     }
 
-    public void animation(View view,ImageView imageView)
-    {
-        ObjectAnimator alphaAnimator=ObjectAnimator.ofFloat(view,"alpha",0f);
+    public void animation(View view, ImageView imageView) {
+        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(view, "alpha", 0f);
         alphaAnimator.setDuration(1500);
-        ObjectAnimator scaleAnimatorX=ObjectAnimator.ofFloat(imageView,"scaleX",0f,(float)(imageView.getWidth()*2));
+        ObjectAnimator scaleAnimatorX = ObjectAnimator.ofFloat(imageView, "scaleX", 0f, (float) (imageView.getWidth() * 2));
         scaleAnimatorX.setDuration(1500);
-        ObjectAnimator scaleAnimatorY=ObjectAnimator.ofFloat(imageView,"scaleY",0f,(float)(imageView.getHeight()*2));
+        ObjectAnimator scaleAnimatorY = ObjectAnimator.ofFloat(imageView, "scaleY", 0f, (float) (imageView.getHeight() * 2));
         scaleAnimatorX.setDuration(1500);
-        AnimatorSet animatorSet= new AnimatorSet();
+        AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(scaleAnimatorX)
                 .with(scaleAnimatorY)
                 .after(alphaAnimator);
